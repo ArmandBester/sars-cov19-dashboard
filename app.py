b16553857298eaa6d9cdef957425a2b722820eb7
Armand Bester <besterpa@ufs.ac.za>
2020-08-26 15:33:00 +0200

    Added slider

--- app.py
+++ app.py
@@ -26,6 +26,8 @@ mkdwn = open('README.md').read()
 
 
 
+
+
 def data_prep():
 
     df = pd.read_csv(source)
@@ -57,9 +59,9 @@ def data_prep():
 # prep the data
 newDf, countries, = data_prep()
 
+
 # create some colours
 colors = px.colors.qualitative.Bold
-
 # Build App
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = 'COVID19 dash'
@@ -72,7 +74,7 @@ app.layout = html.Div(children=[
     dcc.Tabs([
         dcc.Tab(label='Graphics', children=[
 
-            html.H1(children='COVID-19 rates',
+            html.H3(children='COVID-19 rates',
                 style={'textAlign': 'center'}),
 
             html.H5('Countries:'),
@@ -82,16 +84,37 @@ app.layout = html.Div(children=[
                 options=[{'label': v, 'value': v} for v in countries],
                 value=default,
                 multi=True
-            ),
-
-                 
+            ),                       
 
             html.Div([
+                html.H6('Choose polyorder:'),
+                dcc.Slider(
+                    id='poly-slider',
+                    min=1,
+                    max=10,
+                    step=1,
+                    value=2,
+                    dots=True,
+                    marks=dict((i, str(i)) for i in range(1,11,1))
+                ),
+                                
+
                 ## Daily New
                 dcc.Graph(id='Daily_new'),
+                
                 ],style= {'width': '49%', 'display': 'inline-block'}),
             
             html.Div([
+                html.H6('Choose window:'),
+                dcc.Slider(
+                    id='window-slider',
+                    min=7,
+                    max=31,
+                    step=2,
+                    value=21,
+                    dots=True,
+                    marks=dict((i, str(i)) for i in range(7,33,2))
+                ),
                 ## Derivative
                 dcc.Graph(id='d1')
             ],style= {'width': '49%', 'display': 'inline-block'}),    
@@ -116,7 +139,85 @@ app.layout = html.Div(children=[
 ])
 
 
+# Sliders
+#####################################################################################
 
+
+
+
+# Daily new
+#####################################################################################
+@app.callback(
+    Output('Daily_new', 'figure'),
+    [Input('selection', 'value')])
+
+def update_new(default, polyOrder=2):
+    keep = default
+    fig_new = go.Figure()
+    i = 0
+    for c in keep:
+        tmp = newDf[newDf['Country/Region'] == c]
+        fig_new.add_trace(go.Scatter(
+            x=tmp['DaysFromFirstConf'],
+            y=tmp['DailyNewConf'],
+            mode='lines+markers',
+            marker=dict(size=5, color=colors[i]),
+            line = dict(dash='dot'),
+            hovertext=tmp['Date'].dt.date,
+            name=c
+        ))
+
+        fig_new.add_trace(go.Scatter(
+            x=tmp['DaysFromFirstConf'],
+            y=signal.savgol_filter(tmp['DailyNewConf'],
+                                  window_length = 21,
+                                  polyorder=polyOrder, deriv=0),
+            mode='lines',
+            opacity=0.7,
+            marker=dict(size=5, color=colors[i]),
+            hovertext=tmp['Date'].dt.date,
+            name=c
+        ))
+        i += 1
+
+    fig_new.update_layout(template="plotly_white",
+                      title="New COVID-19 cases",
+                      yaxis_title="Daily new",
+                      xaxis_title="Days from first")
+    return fig_new
+
+
+#Delta change
+#####################################################################################
+@app.callback(
+    Output('d1', 'figure'),
+    [Input('selection', 'value')])
+
+def update_d1(default, polyOrder=2):
+    keep = default
+    fig_d1 = go.Figure()
+    i = 0
+    for c in keep:
+        tmp = newDf[newDf['Country/Region'] == c]
+        fig_d1.add_trace(go.Scatter(
+            x=tmp['DaysFromFirstConf'],
+            y=signal.savgol_filter(tmp['DailyNewConf'],
+                                  window_length = 21,
+                                  polyorder=polyOrder, deriv=1),
+            mode='lines+markers',
+            opacity=0.7,
+            marker=dict(size=5, color=colors[i]),
+            hovertext=tmp['Date'].dt.date,
+            name=c
+        ))
+        i += 1
+
+    fig_d1.update_layout(template="plotly_white",
+                      title="First derivative of new COVID-19 cases",
+                      yaxis_title="Change in new cases",
+                      xaxis_title="Days from first")
+
+    return fig_d1
 
 # Cumulative figure
 #####################################################################################
@@ -146,83 +247,6 @@ def update_cum(default):
                       yaxis_title="Cumulative cases",
                       xaxis_title="Days from first")
     return fig_cum
-
-
-# Daily new
-#####################################################################################
-@app.callback(
-    Output('Daily_new', 'figure'),
-    [Input('selection', 'value')])
-
-def update_new(default):
-    keep = default
-    fig_new = go.Figure()
-    i = 0
-    for c in keep:
-        tmp = newDf[newDf['Country/Region'] == c]
-        fig_new.add_trace(go.Scatter(
-            x=tmp['DaysFromFirstConf'],
-            y=tmp['DailyNewConf'],
-            mode='lines+markers',
-            marker=dict(size=5, color=colors[i]),
-            line = dict(dash='dot'),
-            hovertext=tmp['Date'].dt.date,
-            name=c
-        ))
-
-        fig_new.add_trace(go.Scatter(
-            x=tmp['DaysFromFirstConf'],
-            y=signal.savgol_filter(tmp['DailyNewConf'],
-                                  window_length = 21,
-                                  polyorder=2, deriv=0),
-            mode='lines',
-            opacity=0.7,
-            marker=dict(size=5, color=colors[i]),
-            hovertext=tmp['Date'].dt.date,
-            name=c
-        ))
-        i += 1
-
-    fig_new.update_layout(template="plotly_white",
-                      title="New COVID-19 cases",
-                      yaxis_title="Daily new",
-                      xaxis_title="Days from first")
-    return fig_new
-
-
-#Delta change
-#####################################################################################
-@app.callback(
-    Output('d1', 'figure'),
-    [Input('selection', 'value')])
-
-def update_d1(default):
-    keep = default
-    fig_d1 = go.Figure()
-    i = 0
-    for c in keep:
-        tmp = newDf[newDf['Country/Region'] == c]
-        fig_d1.add_trace(go.Scatter(
-            x=tmp['DaysFromFirstConf'],
-            y=signal.savgol_filter(tmp['DailyNewConf'],
-                                  window_length = 21,
-                                  polyorder=2, deriv=1),
-            mode='lines+markers',
-            opacity=0.7,
-            marker=dict(size=5, color=colors[i]),
-            hovertext=tmp['Date'].dt.date,
-            name=c
-        ))
-        i += 1
-
-    fig_d1.update_layout(template="plotly_white",
-                      title="First derivative of new COVID-19 cases",
-                      yaxis_title="Change in new cases",
-                      xaxis_title="Days from first")
-
-    return fig_d1
-
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
